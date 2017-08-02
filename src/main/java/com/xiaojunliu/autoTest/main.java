7f196773e52a1bee3b4c9767c96d6beb1227d934
xiaojunliu@sohu-inc.com <1qaz2wsx>
2017-08-02 22:45:45 +0800

    新建项目

---
+++ src/main/java/com/xiaojunliu/autoTest/main.java
@@ -0,0 +1,10 @@
+package com.xiaojunliu.autoTest;
+
+/**
+ * Created by hp on 2017/8/2.
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println(1);
+    }
+}
