6bf62571257d7389ed3fb7da052ffac9dd593bde
xiaojunliu@sohu-inc.com <1qaz2wsx>
2017-08-06 10:08:36 +0800

    init

--- src/main/java/com/xiaojunliu/autoTest/main.java
+++ src/main/java/com/xiaojunliu/autoTest/main.java
@@ -1,10 +1,25 @@
 package com.xiaojunliu.autoTest;
 
+import io.appium.java_client.android.AndroidDeviceActionShortcuts;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * Created by hp on 2017/8/2.
  */
 public class main {
     public static void main(String[] args){
-        System.out.println(1);
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName","Android");
+
+        try {
+            AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 }
